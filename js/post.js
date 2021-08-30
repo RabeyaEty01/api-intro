7bcce4b68e56086e2c9bb5c44ddd4b4fcef17ae4
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-30 16:36:38 +0600

    Get post added

--- js/post.js
+++ js/post.js
@@ -1,4 +1,4 @@
-
+//GET
 function loadPosts() {
     fetch('http://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
@@ -11,11 +11,28 @@ function displayPosts(posts) {
     for (const post of posts) {
         const div = document.createElement('div');
         div.classList.add('post');
-            div.innerHTML = `
+        div.innerHTML = `
        <h3>${post.title}</h3>
        <p>${post.body}</p>`;
         postContainer.appendChild(div);
 
         console.log(post);
     }
+}
+
+//POST example
+function addAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new Post',
+            body: 'This is my post',
+            userId: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
